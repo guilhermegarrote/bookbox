ef8ddd1d05e1495715b3ee0737f44e492fdbd987
Danilo <dj067337@gmail.com>
2024-08-30 12:19:24 -0300

    Cria Frontend tela de login

--- Sistema/UTelaLogin.pas
+++ Sistema/UTelaLogin.pas
@@ -4,10 +4,24 @@ interface
 
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
-  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs;
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.StdCtrls,
+  FMX.Edit, FMX.Controls.Presentation, FMX.Layouts, FMX.Objects;
 
 type
   TFrmLogin = class(TForm)
+    Layout1: TLayout;
+    RctFundoCinza: TRectangle;
+    RctFundoBranco: TRectangle;
+    Lblbookbox: TLabel;
+    LblNome: TLabel;
+    LblSenha: TLabel;
+    RctBtnEntrar: TRectangle;
+    BtnEntrar: TButton;
+    RctNome: TRectangle;
+    EdtNome: TEdit;
+    RctSenha: TRectangle;
+    EdtSenha: TEdit;
+    procedure BtnEntrarClick(Sender: TObject);
   private
     { Private declarations }
   public
@@ -20,5 +34,10 @@ var
 implementation
 
 {$R *.fmx}
+
+procedure TFrmLogin.BtnEntrarClick(Sender: TObject);
+begin
+  //Abrir próxima tela
+end;
 
 end.
