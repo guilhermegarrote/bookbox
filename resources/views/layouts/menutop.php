f11a95da10e959edda4d5d915d1c4bda08984290
Guilherme Garrote <guilhermegg2008@gmail.com>
2025-02-16 09:29:40 -0300

    Adiciona o link para o css

--- resources/views/layouts/menutop.php
+++ resources/views/layouts/menutop.php
@@ -1,1 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bookbox</title>
+
+    <link rel="stylesheet" href="/Bookbox/public/css/style.css">
+</head>
+<body>
+
 <h1>Menu top</h1>
--- resources/views/pages/login.php
+++ resources/views/pages/login.php
@@ -1,9 +1,11 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+
+    <link rel="stylesheet" href="/Bookbox/public/css/style.css">
 </head>
 
 <body>
