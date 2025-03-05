dfeb527f7d588c1b705970500c1750f11433752e
Guilherme Garrote <guilhermegg2008@gmail.com>
2025-03-04 21:23:25 -0300

    feat(controller): adiciona cadastro e validações no UsuarioController
    - Adiciona a função cadastro() para registrar novos usuários
    - Modifica a função login() para autenticação por e-mail e senha
    - Adiciona a função existeUsuarios() para verificar a existência de
      usuários cadastrados
    - Implementa as funções validarEstruturaNome(), validarEstruturaEmail()
      e validarEstruturaSenha() para validação dos dados de entrada

--- app/Controllers/UsuarioController.php
+++ app/Controllers/UsuarioController.php
@@ -2,6 +2,7 @@
 
 require_once __DIR__ . '/../../config/database.php';
 require_once __DIR__ . '/../Models/UsuarioModel.php';
+require_once __DIR__ . '/../Utils/utils.php';
 
 class UsuarioController
 {
@@ -17,40 +18,142 @@ class UsuarioController
     }
 
     /**
-     * Método responsável por retornar todos os usuarios.
+     * Método responsável por cadastrar usuario.
      * @return void
      */
-    public function listar()
+    public function cadastro()
     {
-        $usuarios = $this->model->listarTodos();
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $nome = $_POST['nome'];
+            $email = $_POST['email'];
+            $senha = $_POST['senha'];
+            $senhaConfirmada = $_POST['senhaConfirmada'];
+
+            if (!$this->validarEstruturaNome($nome)) {
+                adicionarMensagemSessao('nome', 'O nome não é valido. Por favor, tente novamente.');
+            }
+
+            if (!$this->validarEstruturaSenha($senha)) {
+                adicionarMensagemSessao('senha', 'A senha não atende os requisitos. Deve ter de 8 a 16 caracteres, no minimo 1 numero, 1 caracter especial, não deve conter espaços em branco.');
+            }
+
+            if ($senha !== $senhaConfirmada) {
+                adicionarMensagemSessao('senha', 'As senhas não coincidem. Por favor, tente novamente.');
+            }
+
+            if (!$this->validarEstruturaEmail($email)) {
+                adicionarMensagemSessao('email', 'O e-mail fornecido não é válido. Por favor, tente novamente.');
+            }
+
+            if (empty($_SESSION['errors'])) {
+                $cadastroDeuCerto = $this->model->cadastrar($nome, $email, $senha);
+                if ($cadastroDeuCerto) {
+                    header("Location: /bookbox/login");
+                    exit();
+                } else {
+                    adicionarMensagemSessao('geral', 'Erro ao cadastrar usuário. Tente novamente.');
+                }
+            }
+
+            header("Location: /bookbox/cadastro");
+            exit();
+        } else {
+            require_once __DIR__ . '/../resources/views/usuarios/cadastro.php';
+        }
     }
 
     /**
-     * Método responsável por verificar login.
+     * Método responsável por verificar o login.
      * @return void
      */
     public function login()
     {
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            $nome = $_POST['usuNome'];
-            $senha = $_POST['usuSenha'];
+            $email = isset($_POST['email']) ? trim($_POST['email']) : '';
+            $senha = isset($_POST['senha']) ? $_POST['senha'] : '';
 
-            $usuario = $this->model->verificarLogin($nome, $senha);
-
-            if (!empty($usuario) && is_array($usuario)) {
-                $_SESSION['usuario_logado'] = true;
-                $_SESSION['usuario_id'] = $usuario['usuId'];
-                $_SESSION['usuario_nome'] = $usuario['usuNome'];
-
-                header("Location: /bookbox/painel");
-                exit();
-            } else {
-                $_SESSION['erro_login'] = 'Usuário ou senha incorretos!';
-                header("Location: /bookbox/login");
-                exit();
+            if (empty($email)) {
+                adicionarMensagemSessao('email', 'O email é obrigatório.');
             }
+
+            if (!$this->validarEstruturaEmail($email)) {
+                adicionarMensagemSessao('email', 'Email inválido.');
+            }
+
+            if (empty($senha)) {
+                adicionarMensagemSessao('senha', 'A senha é obrigatória.');
+            } else {
+                $usuario = $this->model->verificarLogin($email, $senha);
+
+                if ($usuario) {
+                    $_SESSION['usuario_logado'] = true;
+                    $_SESSION['usuario_nome'] = $usuario['usuNome'];
+                    header("Location: /bookbox/painel");
+                    exit();
+                } else {
+                    adicionarMensagemSessao('senha', 'Email ou senha inválidos.');
+                }
+            }
+
+            header("Location: /bookbox/login");
+            exit();
         } else {
             require_once __DIR__ . '/../resources/views/usuarios/login.php';
         }
+    }
+
+    /**
+     * Método responsável por retornar se existem usuários cadastrados no banco.
+     * @return boolean
+     */
+    public function existeUsuarios()
+    {
+        return $this->model->existeUsuarios();
+    }
+
+    /**
+     * Método responsável por verificar se o nome segue os requisitos mínimos.
+     * @param string $nome
+     * @return boolean
+     */
+    private static function validarEstruturaNome($nome)
+    {
+        if (preg_match("/^[A-Za-zÀ-ÖØ-öø-ÿ' -]+$/", trim($nome))) {
+            $palavras = explode(" ", trim($nome));
+            if (count($palavras) >= 2 && strlen($nome) >= 3 && strlen($nome) <= 100) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Método responsável por validar o estrutura do e-mail.
+     * @param string $email
+     * @return boolean
+     */
+    private static function validarEstruturaEmail($email)
+    {
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            adicionarMensagemSessao('email', 'O e-mail fornecido tem formato inválido.');
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Método responsável por verificar se a estrutura da senha segue os requisitos mínimos.
+     * @param string $senha
+     * @return boolean
+     */
+    private static function validarEstruturaSenha($senha)
+    {
+        if (
+            preg_match("/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&(),.?\":{}|<>])[A-Za-z\d!@#$%^&(),.?\":{}|<>]{8,16}$/", $senha)
+        ) {
+            return true;
+        }
+        return false;
     }
 }
