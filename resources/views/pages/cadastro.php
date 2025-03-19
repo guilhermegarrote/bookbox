1f3bbfd932e7ea20faf633a04bd29f089cfc00ff
Conrado <baladelic@gmail.com>
2025-03-19 20:20:56 -0300

    Adiciona o design da tela de cadastro

--- resources/views/pages/cadastro.php
+++ resources/views/pages/cadastro.php
@@ -1,31 +1,40 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro</title>
 
     <link rel="stylesheet" href="/bookbox/public/css/style.css">
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Sancreek&family=Sedgwick+Ave+Display&family=Urbanist:ital,wght@0,100..900;1,100..900&display=swap');
+    </style>
 </head>
 
-<body>
-    <h2>Cadastro</h2>
+<body class="cadastro-container">
+    <div class="cadastro-box">
+        <!-- 🔹 Formulário de cadastro -->
+        <div class="cadastro-form">
+            <h2>Cadastro</h2>
 
-    <form action="cadastro" method="POST">
-        <label>Nome e sobrenome:</label>
-        <input type="text" name="nome" required>
+            <form action="cadastro" method="POST">
+                <label class="cadastro-label">Nome e sobrenome:</label>
+                <input type="text" name="nome" class="cadastro-input" required>
 
-        <label>Email:</label>
-        <input type="email" name="email" required>
+                <label class="cadastro-label">Email:</label>
+                <input type="email" name="email" class="cadastro-input" required>
 
-        <label>Senha:</label>
-        <input type="password" name="senha" required>
+                <label class="cadastro-label">Senha:</label>
+                <input type="password" name="senha" class="cadastro-input" required>
 
-        <label>Confirmar senha:</label>
-        <input type="password" name="senhaConfirmada" required>
+                <label class="cadastro-label">Confirmar senha:</label>
+                <input type="password" name="senhaConfirmada" class="cadastro-input" required>
 
-        <button type="submit">Entrar</button>
-    </form>
+                <button type="submit" class="cadastro-button">→</button>
+            </form>
+        </div>
+    </div>
 </body>
 
 </html>
