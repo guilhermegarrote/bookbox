9f08d22ff4880e4ab471abff864d754a8d94171c
Ana Beatriz <anabfogacamedeiros@gmail.com>
2025-03-28 20:50:02 -0300

    Adiciona arquivos ao LivroController.php

---
+++ app/Controllers/LivroController.php
@@ -0,0 +1,167 @@
+<?php
+
+require_once __DIR__ . '/../../config/database.php';
+require_once __DIR__ . '/../Models/LivroModel.php';
+require_once __DIR__ . '/../Models/GeneroModel.php';
+require_once __DIR__ . '/../Models/ExemplarModel.php';
+require_once __DIR__ . '/../Utils/utils.php';
+
+class EmprestimoController
+{
+    private $livroModel;
+    private $generoModel;
+    private $exemplarModel;
+
+    /**
+     * Construtor da classe, responsável por inicializar os modelos e estabelecer a conexão com o banco de dados.
+     * @return void
+     */
+    public function __construct()
+    {
+        $db = Database::conectar();
+        $this->livroModel = new LivroModel($db);
+        $this->generoModel = new GeneroModel($db);
+        $this->exemplarModel = new ExemplarModel($db);
+    }
+
+    /**
+     * Método responsável por cadastrar aluno.
+     * @return void
+     */
+    public function cadastro() 
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $Ibsn = $_POST['Ibsn'];
+            $titulo = $_POST['titulo'];
+            $autor = $_POST['autor'];
+            $generoId = $_POST['generoId'];
+            $editora = $_POST['editora'];
+
+            $erros = [];
+
+            if (empty($Ibsn)) {
+                $erros['Ibsn'] = 'O código IBSN é obrigatório.';
+            } elseif (!$this->validarEstruturaIbsn($Ibsn)) {
+                $erros['Ibsn'] = 'O código IBSN fornecido não é válido. Por favor, tente novamente.';
+            }
+
+           if (empty($titulo)) {
+                $erros['titulo'] = 'O título é obrigatório.';
+             }elseif (!$this->validarEstruturaTitulo($titulo)) {
+                $erros['titulo'] = 'O título fornecido não é válido. Por favor, tente novamente.';
+            }
+
+            if (empty($autor)) {
+                $erros['autor'] = 'O nome do autor é obrigatório.';
+            }elseif (!$this->validarEstruturaAutor($autor)) {
+                $erros['autor'] = 'O nome do autor fornecido não é válido. Por favor, tente novamente.';
+            }
+
+            if (empty($generoId)) {
+                $erros['generoId'] = 'O ID do gênero é obrigatório.';
+            }
+
+            if (empty($editora)) {
+                $erros['editora'] = 'A editora é obrigatória.';
+            }elseif (!$this->validarEstruturaEditora($editora)) {
+                $erros['editora'] = 'A editora fornecida não é válida. Por favor, tente novamente.';
+            }
+
+            if (!empty($erros)) {
+                echo json_encode(["erro" => $erros]);
+                http_response_code(400);
+                exit();
+            }
+
+            $generoExiste = $this->generoModel->buscaExemplar($generoId);
+            if (!$generoExiste) {
+                $erros['generoId'] = 'O gênero informado não está cadastrado. Por favor, verifique e tente novamente.';
+                echo json_encode(["erro" => $erros]);
+                http_response_code(400);
+                exit();
+            }
+
+            $cadastroDeuCerto = $this->livroModel->cadastrar($Ibsn, $titulo, $autor, $generoId, $editora);
+            if ($cadastroDeuCerto) {
+                echo json_encode([
+                    "mensagem" => "Livro cadastrado com sucesso!",
+                    "redirecionar" => "/bookbox/livros"
+                ]);
+                exit();
+            } else {
+                $erros['geral'] = 'Erro ao cadastrar livro. Tente novamente.';
+                echo json_encode(["erro" => $erros]);
+                http_response_code(500);
+                exit();
+            }
+        } else {
+            require_once __DIR__ . '/../resources/views/alunos/cadastro.php';
+        }
+    }
+
+    /**
+     * Método responsável por verificar se o IBSN segue os requisitos mínimos.
+     * @param string $Ibsn
+     * @return boolean
+     */
+    private static function validarEstruturaIbsn($Ibsn)
+    {
+        if (preg_match("/^(?:\d{9}[\dX]|\d{13})$/", trim($Ibsn))) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Método responsável por verificar se o titulo segue os requisitos mínimos.
+     * @param string $titulo
+     * @return boolean
+     */
+    private static function validarEstruturaTitulo($titulo)
+    {
+        if (preg_match("/^[\p{L}\p{N}\p{P}\p{Zs}]+$/", trim($titulo))) {
+            $palavras = explode(" ", trim($titulo));
+            if (count($palavras) >= 2 && strlen($titulo) >= 3 && strlen($titulo) <= 100) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Método responsável por verificar se o nome do autor segue os requisitos mínimos.
+     * @param string $autor
+     * @return boolean
+     */
+    private static function validarEstruturaAutor($autor)
+    {
+        if (preg_match("/^[A-Za-zÀ-ÖØ-öø-ÿ' -]+$/", trim($autor))) {
+            $palavras = explode(" ", trim($autor));
+            if (count($palavras) >= 2 && strlen($autor) >= 3 && strlen($autor) <= 100) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Método responsável por verificar se o nome da editora segue os requisitos mínimos.
+     * @param string $editora
+     * @return boolean
+     */
+    private static function validarEstruturaEditora($editora)
+    {
+        if (preg_match("/^[A-Za-z0-9\s\.\-]+$/", trim($editora))) {
+            $palavras = explode(" ", trim($editora));
+            if (count($palavras) >= 2 && strlen($editora) >= 3 && strlen($editora) <= 100) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+}
