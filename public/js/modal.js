fb0ff57ab787a3818ac7c8a0e58cae19ee8e3097
Guilherme Garrote <guilhermegg2008@gmail.com>
2025-04-16 21:17:00 -0300

    feat(js): adiciona auto preenchimento da data de devolução no modal.js
    - Preenche automaticamente o campo de data de devolução ao abrir o modal

--- public/js/modal.js
+++ public/js/modal.js
@@ -3,12 +3,16 @@ function abrirModal(modalType, id = null) {
     if (id) {
         url += `?id=${id}`;
     }
-    
+
     fetch(url)
         .then(response => response.text())
         .then(html => {
             document.getElementById("modal-container").innerHTML = html;
             document.getElementById("overlay").style.display = "flex";
+
+            if (modalType === 'cadastro_emprestimo') {
+                preencherDataDevolucao('dataDevolucao');
+            }
         })
         .catch(error => console.error('Erro ao carregar o modal:', error));
 }
@@ -18,7 +22,7 @@ function fecharModal() {
     document.getElementById("modal-container").innerHTML = '';
 }
 
-window.onclick = function(event) {
+window.onclick = function (event) {
     if (event.target === document.getElementById("overlay")) {
         fecharModal();
     }
@@ -26,25 +30,20 @@ window.onclick = function(event) {
 
 function realizarEmprestimo(event) {
     event.preventDefault();
-    
+
     const formData = new FormData();
-    formData.append("nomeEstudante", document.getElementById("nomeEstudante").value);
     formData.append("cpf", document.getElementById("cpf").value);
-    formData.append("serie", document.getElementById("serie").value);
-    formData.append("curso", document.getElementById("curso").value);
-    formData.append("nomeLivro", document.getElementById("nomeLivro").value);
     formData.append("codigoIBSN", document.getElementById("codigoIBSN").value);
-    formData.append("dataDevolucao", document.getElementById("dataDevolucao").value);
     formData.append("exemplar", document.getElementById("exemplar").value);
-    
+
     fetch('/bookbox/api/cadastrar_emprestimo', {
         method: 'POST',
         body: formData
     })
-    .then(response => response.json())
-    .then(data => {
-        alert(data.mensagem);
-        fecharModal();
-    })
-    .catch(error => console.error('Erro ao cadastrar:', error));
+        .then(response => response.json())
+        .then(data => {
+            alert(data.mensagem);
+            fecharModal();
+        })
+        .catch(error => console.error('Erro ao cadastrar:', error));
 }
