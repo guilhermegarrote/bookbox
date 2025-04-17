093a76e4d15d9dcabecc727975e9731bf6c2773c
Guilherme Garrote <guilhermegg2008@gmail.com>
2025-04-16 21:20:48 -0300

    feat(ui): melhora modais com ícone de fechar, máscaras e validações
    - Substitui caractere "X" por ícone no botão de fechar
    - Aplica máscaras nos campos de CPF, IBSN e campos numéricos
    - Organiza validações com atributos required

--- resources/views/modals/cadastro_emprestimo.php
+++ resources/views/modals/cadastro_emprestimo.php
@@ -1,54 +1,53 @@
 <div id="modal" class="modal">
     <div class="modal-header">
         <h2 class="modal-title">Cadastro de Empréstimo</h2>
-        <button onclick="fecharModal()" class="modal-close">X</button>
+        <button onclick="fecharModal()" class="modal-close">
+            <i class="fas fa-times"></i>
+        </button>
     </div>
     <div class="modal-body">
         <div class="rec-inputs">
             <div class="entryarea">
-                <input type="text" id="nomeEstudante" class="modal-input" style="width: 450px;" readonly required>
+                <input type="text" id="nomeEstudante" class="modal-input" style="width: 450px;" readonly>
                 <label class="labelline" for="nomeEstudante">Nome do Estudante</label>
             </div>
             <div class="entryarea">
-                <input type="text" id="status" class="modal-input" style="width: 140px;" readonly required>
+                <input type="text" id="status" class="modal-input" style="width: 140px;" readonly>
                 <label class="labelline" for="status">Status do aluno</label>
             </div>
-
-            <div class="entryarea" >
-                <input type="text" id="cpf" class="modal-input" style="width:140px;"required>
+            <div class="entryarea">
+                <input type="text" id="cpf" class="modal-input" oninput="mascararCPF(this)" style="width: 140px;" required>
                 <label class="labelline" for="cpf">CPF</label>
             </div>
             <div class="entryarea">
-                <input type="text" id="serie" class="modal-input" style="width:123px;" readonly required>
+                <input type="text" id="serie" class="modal-input" style="width: 123px;" readonly>
                 <label class="labelline" for="serie">Período</label>
             </div>
-            <div class="entryarea ">
-                <input type="text" id="curso" class="modal-input"  style="width:300px;" readonly required>
+            <div class="entryarea">
+                <input type="text" id="curso" class="modal-input" style="width: 300px;" readonly>
                 <label class="labelline" for="curso">Curso</label>
             </div>
-
             <div class="entryarea">
-                <input type="text" id="codigoIBSN" class="modal-input" style="width:170px;" required>
+                <input type="text" id="codigoIBSN" class="modal-input" oninput="mascararISBN(this)" style="width: 170px;" required>
                 <label class="labelline" for="codigoIBSN">Código IBSN</label>
             </div>
             <div class="entryarea">
-                <input type="text" id="nomeLivro" class="modal-input" style="width:420px;" readonly required>
-                <label class="labelline" for="nomeLivro">Nome do Livro</label>
+                <input type="text" id="titulo" class="modal-input" style="width: 420px;" readonly>
+                <label class="labelline" for="titulo">Título</label>
             </div>
             <div class="entryarea">
-                <input type="text" id="statusl" class="modal-input" style="width:140px;" readonly required>
+                <input type="text" id="numeroExemplar" class="modal-input" oninput="permitirSomenteNumeros(this)" style="width: 140px;" required>
+                <label class="labelline" for="numeroExemplar">Exemplar</label>
+            </div>
+            <div class="entryarea">
+                <input type="text" id="statusl" class="modal-input" style="width: 140px;" readonly>
                 <label class="labelline" for="statusl">Status do livro</label>
             </div>
-            <div class="entryarea" style="right:8%;" >
-                <input type="text" id="exemplar" class="modal-input" style="width:140px;" readonly required>
-                <label class="labelline" for="exemplar">Exemplar</label>
-            </div>
-            <div class="entryarea" style="right:16%;">
-                <input type="text" id="dataDevolucao" class="modal-input" style="width:170px;" readonly required>
+            <div class="entryarea">
+                <input type="text" id="dataDevolucao" class="modal-input" style="width: 170px;" readonly>
                 <label class="labelline" for="dataDevolucao">Data de Devolução</label>
             </div>
         </div>
-
         <div class="modal-footer">
             <button onclick="fecharModal()" class="modal-button">Cancelar</button>
             <button onclick="realizarEmprestimo(event)" class="modal-button">Realizar Empréstimo</button>
--- resources/views/modals/cadastro_livro.php
+++ resources/views/modals/cadastro_livro.php
@@ -1,37 +1,37 @@
 <div id="modal" class="modal">
     <div class="modal-header">
         <h2 class="modal-title">Cadastro de Livro</h2>
-        <button onclick="fecharModal()" class="modal-close">X</button>
+        <button onclick="fecharModal()" class="modal-close">
+            <i class="fas fa-times"></i>
+        </button>
     </div>
     <div class="modal-body">
         <div class="rec-inputs">
             <div class="entryarea">
-                <input type="text" id="codigo-ibns" class="modal-input" style="width:170px;"  required>
-                <label class="labelline" for="codigo-ibns">Código IBSN</label>
+                <input type="text" id="codigoIBSN" class="modal-input" oninput="mascararISBN(this)" style="width: 170px;" required>
+                <label class="labelline" for="codigoIBSN">Código IBSN</label>
             </div>
             <div class="entryarea">
-                <input type="text" id="titulo" class="modal-input" style="width:420px;" required>
+                <input type="text" id="titulo" class="modal-input" style="width: 420px;" required>
                 <label class="labelline" for="titulo">Título</label>
             </div>
-
-            <div class="entryarea" >
-                <input type="text" id="editora" class="modal-input" style="width:340px;"required>
+            <div class="entryarea">
+                <input type="text" id="editora" class="modal-input" style="width: 340px;" required>
                 <label class="labelline" for="editora">Editora</label>
             </div>
             <div class="entryarea">
-                <input type="text" id="autor" class="modal-input" style="width:250px;"  required>
+                <input type="text" id="autor" class="modal-input" style="width: 250px;" required>
                 <label class="labelline" for="autor">Autor</label>
             </div>
-            <div class="entryarea" style=" width:175px;">
-                <input type="text" id="genero" class="modal-input"  style="width:150px;"  required>
+            <div class="entryarea" style="width: 175px;">
+                <input type="text" id="genero" class="modal-input" style="width: 150px;" required>
                 <label class="labelline" for="genero">Gênero</label>
 
-                <div style=" position: absolute; width: 35px; height: 35px;  top: 60%; left: 84.8%; border: 1px solid #ccc; border-radius: 6px;  transform: translateY(-60%); background-color: blue;"></div>
+                <div style="position: absolute; width: 35px; height: 35px;  top: 60%; left: 84.8%; border: 1px solid #ccc; border-radius: 6px;  transform: translateY(-60%); background-color: blue;"></div>
             </div>
-
             <div class="entryarea" style="right:25%;">
-                <input type="text" id="quantidade de exemplares" class="modal-input" style="width:230px;" required>
-                <label class="labelline" for="quantidade de exemplares">Quantidade de exemplares</label>
+                <input type="text" id="quantidadeExemplares" class="modal-input" oninput="permitirSomenteNumeros(this)" style="width: 230px;" required>
+                <label class="labelline" for="quantidadeExemplares">Quantidade de exemplares</label>
             </div>
         </div>
 
