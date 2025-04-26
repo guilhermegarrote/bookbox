12d344c538f410c23e5262b741705190e39555d3
Guilherme Garrote <guilhermegg2008@gmail.com>
2025-04-26 18:30:03 -0300

    refactor(modals): reorganiza modais e separa JS de HTML
    - Reorganiza os modais de cadastro de aluno, empréstimo e livro em
      pastas separadas
    - Remove o código JS integrado nos arquivos HTML dos modais
    - Separa a lógica de JS dos arquivos HTML, melhorando a manutenção e
      organização

--- resources/views/modals/livros/cadastro_livro.php
+++ resources/views/modals/livros/cadastro_livro.php
@@ -1,15 +1,15 @@
 <div id="modal" class="modal">
     <div class="modal-header">
         <h2 class="modal-title">Cadastro de Livro</h2>
-        <button onclick="fecharModal()" class="modal-close">
+        <button id="botao-fechar-modal" class="modal-close">
             <i class="fas fa-times"></i>
         </button>
     </div>
     <div class="modal-body">
         <div class="rec-inputs">
             <div class="entryarea">
-                <input type="text" id="codigoIBSN" class="modal-input" oninput="mascararISBN(this)" style="width: 170px;" required>
-                <label class="labelline" for="codigoIBSN">Código IBSN</label>
+                <input type="text" id="isbn" class="modal-input" style="width: 170px;" required>
+                <label class="labelline" for="isbn">ISBN</label>
             </div>
             <div class="entryarea">
                 <input type="text" id="titulo" class="modal-input" style="width: 420px;" required>
@@ -24,20 +24,19 @@
                 <label class="labelline" for="autor">Autor</label>
             </div>
             <div class="entryarea" style="width: 175px;">
-                <input type="text" id="genero" class="modal-input" style="width: 150px;" required>
+                <input type="text" id="genero" class="modal-input" style="width: 150px; border-radius: 6px 0px 0px 6px;" required>
                 <label class="labelline" for="genero">Gênero</label>
-
-                <div style="position: absolute; width: 35px; height: 35px;  top: 60%; left: 84.8%; border: 1px solid #ccc; border-radius: 6px;  transform: translateY(-60%); background-color: blue;"></div>
+                <div style="position: absolute; width: 35px; height: 35px; top: 60%; left: 84.8%; border: 1px solid #ccc; border-radius: 0px 6px 6px 0px; transform: translateY(-60%); background-color: blue;"></div>
             </div>
-            <div class="entryarea" style="right:25%;">
-                <input type="text" id="quantidadeExemplares" class="modal-input" oninput="permitirSomenteNumeros(this)" style="width: 230px;" required>
-                <label class="labelline" for="quantidadeExemplares">Quantidade de exemplares</label>
+            <div class="entryarea" style="right: 25%;">
+                <input type="text" id="quantidade-exemplares" class="modal-input" style="width: 230px;" required>
+                <label class="labelline" for="quantidade-exemplares">Quantidade de exemplares</label>
             </div>
         </div>
 
         <div class="modal-footer">
-            <button onclick="fecharModal()" class="modal-button">Cancelar</button>
-            <button onclick="cadastrarLivro(event)" class="modal-button">Cadastrar livro</button>
+            <button id="botao-fechar-modal" class="modal-button">Cancelar</button>
+            <button class="modal-button">Cadastrar</button>
         </div>
     </div>
 </div>
