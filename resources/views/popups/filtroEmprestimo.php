5c5c31a3a655f9bb1078a05dad4e9ad717a53e28
Conrado <baladelic@gmail.com>
2025-06-02 19:19:17 -0300

    Implementados filtros nas telas correspondentes, permitindo ao usuário refinar os resultados por tipo de empréstimo e por aluno

---
+++ resources/views/popups/filtroEmprestimo.php
@@ -0,0 +1,6 @@
+<div class="filtro-popup filtro-popup-emprestimo">
+    <label class="titulo-filtro">Filtrar por:</label>
+
+    <button class="botao-filtro">Pendente</button>
+    <button class="botao-filtro">Finalizado</button>
+</div>
