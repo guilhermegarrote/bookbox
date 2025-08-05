1f5f6fbc60f6ec9fd8c0accb01a445dd49cdce4f
Guilherme Garrote <guilhermegg2008@gmail.com>
2025-08-04 23:06:44 -0300

    refactor(build): remove imports dinâmicos de CSS e JS do app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,18 +1,1 @@
 import './bootstrap';
-
-// Isso carrega todos os arquivos CSS do diretório e subdiretórios
-const cssModules = import.meta.glob([
-  '../css/**/*.css',
-  '../css/*.css'
-]);
-
-// Importa todos os arquivos encontrados
-Object.values(cssModules).forEach(importFn => importFn());
-
-const jsModules = import.meta.glob([
-  './**/*.js',
-  './**/**/*.js',
-]);
-
-// Importa todos os arquivos encontrados
-Object.values(jsModules).forEach(importFn => importFn());
