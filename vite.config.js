e06fb16a4769a5d99cc90ee56d256de7c55d0e71
Guilherme Garrote <guilhermegg2008@gmail.com>
2025-08-04 22:58:07 -0300

    feat(build): adicionar importação dinâmica de scripts no vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,21 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import fg from 'fast-glob';
+
+const jsApi = fg.sync('resources/js/api/**/*.js');
+const jsComponents = fg.sync('resources/js/components/**/*.js');
+const jsPages = fg.sync('resources/js/pages/**/*.js');
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+                ...jsApi,
+                ...jsComponents,
+                ...jsPages,
+            ],
             refresh: true,
         }),
     ],
