8373289028f9d98ea5e6a44d13b61f338ffd66a8
Guilherme Garrote <guilhermegg2008@gmail.com>
2025-08-04 22:45:31 -0300

    chore(modal): remove botão comentado de fechamento do modal
    Removido botão comentado que não era mais utilizado no modal.blade.php.

--- resources/views/components/modals/modal.blade.php
+++ resources/views/components/modals/modal.blade.php
@@ -3,9 +3,6 @@
 <div id="{{ $id }}" class="modal-window" aria-hidden="true">
     <div class="modal-header">
         <h2 class="modal-title">{{ $title }}</h2>
-        <!-- <button type="button" class="modal-close-btn" id="{{ $closeId }}" aria-label="Fechar modal">
-             <x-icons.icon name="close" class="close-icon"/>
-        </button> -->
     </div>
 
     <div class="modal-content">
