989cc856b2c22a09bd72f931952f69d2086e51cf
Ana Julia Miura <anajuliamoralesmiura@gmail.com>
2025-08-12 10:48:17 -0300

    chore(Requests): Corrige o nome do Update e Store da Request, e move os arquivos pra pasta de requests

--- app/Http/Requests/Book/BookUpdateRequest.php
+++ app/Http/Requests/Book/BookUpdateRequest.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Requests\Book;
 
+use App\Helpers\Utils;
 use Illuminate\Foundation\Http\FormRequest;
 
-class BookClassStoreRequest extends FormRequest
+class BookUpdateRequest extends FormRequest
 {
     public function authorize(): bool
     {
@@ -18,6 +19,9 @@ class BookClassStoreRequest extends FormRequest
             'title' => trim($this->input('title', '')),
             'author' => trim($this->input('author', '')),
             'publisher' => trim($this->input('publisher', '')),
+            'genre_id' => $this->has('genre_id') && $this->input('genre_id') !== null
+                ? Utils::convertUuidToBinary($this->input('genre_id'))
+                : null,
         ]);
     }
 
@@ -25,38 +29,33 @@ class BookClassStoreRequest extends FormRequest
     {
         return [
             'isbn' => [
-                'required',
+                'sometimes',
                 'string',
                 'max:20',
                 'regex:/^(97(8|9))?\d{9}(\d|X)$/i'
             ],
-            'title' => ['required', 'string', 'max:255'],
-            'author' => ['required', 'string', 'max:255'],
-            'genre_id' => ['required', 'exists:genres,id'],
-            'publisher' => ['required', 'string', 'max:255'],
+            'title' => ['sometimes', 'string', 'max:255'],
+            'author' => ['sometimes', 'string', 'max:255'],
+            'genre_id' => ['sometimes', 'exists:genres,id'],
+            'publisher' => ['sometimes', 'string', 'max:255'],
         ];
     }
 
     public function messages(): array
     {
         return [
-            'isbn.required' => 'O ISBN é obrigatório.',
             'isbn.string' => 'O ISBN deve ser um texto.',
             'isbn.max' => 'O ISBN não pode ter mais que 20 caracteres.',
             'isbn.regex' => 'O ISBN informado não é válido.',
 
-            'title.required' => 'O título é obrigatório.',
             'title.string' => 'O título deve ser um texto.',
             'title.max' => 'O título não pode ter mais que 255 caracteres.',
 
-            'author.required' => 'O autor é obrigatório.',
             'author.string' => 'O autor deve ser um texto.',
             'author.max' => 'O nome do autor não pode ter mais que 255 caracteres.',
 
-            'genre_id.required' => 'O gênero é obrigatório.',
             'genre_id.exists' => 'O gênero selecionado não existe.',
 
-            'publisher.required' => 'A editora é obrigatória.',
             'publisher.string' => 'A editora deve ser um texto.',
             'publisher.max' => 'O nome da editora não pode ter mais que 255 caracteres.',
         ];
