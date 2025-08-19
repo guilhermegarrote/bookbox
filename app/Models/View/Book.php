20bf426b4402a75d5c283980908c94572279e778
Danju <dj067337@gmail.com>
2025-08-19 11:08:32 -0300

    feat(model): adiciona model da view de livros

---
+++ app/Models/View/Book.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models\View;
+
+use App\Models\Copy;
+use App\Models\Genre;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Book extends BaseModel
+{
+    protected $table = 'vw_books';
+    public $timestamps = false;
+
+    protected $guarded = [];
+
+    public function genre(): BelongsTo
+    {
+        return $this->belongsTo(Genre::class);
+    }
+
+    public function copies(): HasMany
+    {
+        return $this->hasMany(Copy::class);
+    }
+}
