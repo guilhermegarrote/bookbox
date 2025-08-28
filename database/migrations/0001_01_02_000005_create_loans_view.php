4d0d424f977f91ef95c564c24963afe8c2c07ab1
Guilherme Garrote <guilhermegg2008@gmail.com>
2025-08-28 10:25:13 -0300

    feat(migrations): adicionar migration para view de empréstimos

---
+++ database/migrations/0001_01_02_000005_create_loans_view.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+class CreateLoansView extends Migration
+{
+    public function up(): void
+    {
+        $this->down();
+        DB::statement("
+            CREATE VIEW vw_loans AS
+            SELECT
+                l.id AS id,
+                c.id AS copy_id,
+                c.isbn AS isbn,
+                c.title AS title,
+                c.author AS author,
+                c.genre_id AS genre_id,
+                c.genre_name AS genre_name,
+                c.genre_color_hex AS genre_color_hex,
+                c.publisher AS publisher,
+                c.number AS number,
+                ssc.student_id AS student_id,
+                ssc.name AS name,
+                ssc.cpf AS cpf,
+                ssc.cpf_hash AS cpf_hash,
+                ssc.email AS email,
+                ssc.email_hash AS email_hash,
+                ssc.phone AS phone,
+                ssc.phone_hash AS phone_hash,
+                ssc.can_borrow AS can_borrow,
+                ssc.school_class_id AS school_class_id,
+                ssc.course AS course,
+                ssc.term AS term,
+                ssc.start_date AS start_date,
+                ssc.end_date AS end_date,
+                ssc.period AS period,
+                ssc.formatted_class_name AS formatted_class
+            FROM loans l
+            LEFT JOIN vw_copies c ON c.id = l.copy_id
+            LEFT JOIN vw_student_school_class ssc ON ssc.student_id = l.student_id
+        ");
+    }
+
+    public function down(): void
+    {
+        DB::statement('DROP VIEW IF EXISTS vw_loans');
+    }
+}
