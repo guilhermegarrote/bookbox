1cfad5c3be006184eb6717ef6a66216f193ab47f
Ana Beatriz <anabfogacamedeiros@gmail.com>
2025-09-02 12:02:26 -0300

    Adiciona no controller de exemplares a função de cadastro

--- app/Http/Controllers/API/CopyController.php
+++ app/Http/Controllers/API/CopyController.php
@@ -2,47 +2,55 @@
 
 namespace App\Http\Controllers\API;
 
-use Illuminate\Http\Request;
+use App\Helpers\Utils;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Copy\CopyStoreRequest;
+use App\Models\Book;
+use App\Models\Copy;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\DB;
+use Throwable;
 
-class CopyController
+class CopyController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Store new copies linked to a book via ISBN.
      */
-    public function index()
+    public function store(CopyStoreRequest $request): JsonResponse
     {
-        //
-    }
+        DB::beginTransaction();
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+        try {
+            // Find book by ISBN
+            $book = Book::where('isbn', $request->isbn)->first();
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
+            if (!$book) {
+                return response()->json([
+                    'message' => 'Livro não encontrado para o ISBN informado.'
+                ], 404);
+            }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
+            $copies = [];
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+            // Create the requested number of copies
+            for ($i = 0; $i < $request->numberOfCopies; $i++) {
+                $copies[] = Copy::create([
+                    'book_id'   =>  Utils::convertUuidToBinary($book->id),
+                    'number'    => $i+1,
+                    'available' => $request->available ?? true,
+                ]);
+            }
+
+            DB::commit();
+
+             return $this->createdResponse();
+
+        } catch (Throwable $e) {
+            DB::rollBack();
+
+            $this->logError('Erro ao cadastrar exemplar.', $e, ['book' => $book]);
+            return $this->internalErrorResponse($e, 'Erro interno ao cadastrar exemplar.');
+
+        }
     }
 }
