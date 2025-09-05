ddba35ce64efbbcd8b871d597333a5f51cd6b5f3
Ana Julia Miura <anajuliamoralesmiura@gmail.com>
2025-09-05 18:28:07 -0300

    feat(Requests): melhora codigo regex para validação mais eficaz

--- app/Http/Requests/Copy/CopyStoreRequest.php
+++ app/Http/Requests/Copy/CopyStoreRequest.php
@@ -23,7 +23,13 @@ class CopyStoreRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'isbn' => ['required', 'string', 'max:20'],
+            'isbn' => [
+                'required',
+                'string',
+                'max:20',
+                'regex:/^(97(8|9))?\d{9}(\d|X)$/i',
+                'exists:books,isbn',
+            ],
             'numberOfCopies' => ['required', 'integer', 'min:1'],
             'available' => ['sometimes', 'boolean'],
         ];
@@ -35,6 +41,8 @@ class CopyStoreRequest extends FormRequest
             'isbn.required' => 'O ISBN é obrigatório.',
             'isbn.string' => 'O ISBN deve ser um texto.',
             'isbn.max' => 'O ISBN não pode ter mais que 20 caracteres.',
+            'isbn.regex' => 'O ISBN informado não tem um formato válido.',
+            'isbn.exists' => 'O ISBN informado não foi encontrado na base de livros.',
 
             'numberOfCopies.required' => 'O número de exemplares é obrigatório.',
             'numberOfCopies.integer' => 'O número de exemplares deve ser um número inteiro.',
