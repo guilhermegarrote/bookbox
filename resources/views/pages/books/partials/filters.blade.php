5ec8e7b00fe5977726392438557e66f54b4a0497
Danju <dj067337@gmail.com>
2025-09-16 11:45:05 -0300

    Adiciona o filtro de livros e emprestimos

---
+++ resources/views/pages/books/partials/filters.blade.php
@@ -0,0 +1,15 @@
+<div>
+    <div class="filter-header">
+        <label class="filter-title">Filtrar por:</label>
+        <button id="filter-clean-btn" class="filter-clean-btn" title="Limpar todos os filtros">
+            <x-icons.icon name="broom" />
+        </button>
+    </div>
+
+    <div>
+        <div class="filter-row">
+            <select id="filter-genre_name" class="filter-input" name="genre_name" title="Selecione o Gênero"></select>
+            <select id="filter-publisher" class="filter-input" name="publisher" title="Selecione a Editora "></select>
+        </div>
+    </div>
+</div>
---
+++ resources/views/pages/loans/partials/filters.blade.php
@@ -0,0 +1,18 @@
+<div>
+    <div class="filter-header">
+        <label class="filter-title">Filtrar por:</label>
+        <button id="filter-clean-btn" class="filter-clean-btn" title="Limpar todos os filtros">
+            <x-icons.icon name="broom" />
+        </button>
+    </div>
+
+    <div>
+        <div class="filter-row">
+            <select id="filter-genre_name" class="filter-input" name="genre_name" title="Selecione o Gênero"></select>
+            <select id="filter-publisher" class="filter-input" name="publisher" title="Selecione a Editora"></select>
+            <select id="filter-course" class="filter-input" name="course" title="Selecione o Curso"></select>
+            <select id="filter-period" class="filter-input" name="period" title="Selecione o Periodo"></select>
+            <select id="filter-ternm" class="filter-input" name="ternm" title="Selecione o Termo"></select>
+        </div>
+    </div>
+</div>
