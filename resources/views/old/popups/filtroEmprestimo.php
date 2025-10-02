d0a5fb3274dbcd1485543444117f84adf3713b50
Guilherme Garrote <guilhermegg2008@gmail.com>
2025-10-01 23:03:27 -0300

    chore(cleanup): remove arquivos antigos da migração e email duplicado
    Removidos arquivos da pasta 'old', referentes à versão anterior em PHP puro, que foram mantidos temporariamente para migração.
    Também foi removido o arquivo de email antigo (password_reset.blade.php), que estava duplicado e foi substituído pelo arquivo versionado corretamente.
    Essa limpeza mantém o repositório organizado e evita confusão com arquivos obsoletos.

--- resources/views/old/popups/filtroEmprestimo.php
+++
@@ -1,6 +0,0 @@
-<div class="filtro-popup filtro-popup-emprestimo">
-    <label class="titulo-filtro">Filtrar por:</label>
-
-    <button class="botao-filtro">Pendente</button>
-    <button class="botao-filtro">Finalizado</button>
-</div>
