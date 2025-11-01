d968934713c6259419f5d40159455f0888cf41f9
Guilherme Garrote <guilhermegg2008@gmail.com>
2025-11-01 16:28:38 -0300

    feat(pages): adiciona .blade do modal de gerar etiquetas

---
+++ resources/views/pages/labels/partials/generate-label-modal.blade.php
@@ -0,0 +1,10 @@
+<x-modals.modal id="labelGenerateModal" title="Gerar etiquetas">
+    <x-slot name="content">
+    </x-slot name="content">
+
+    <x-slot name="footer">
+        <button type="button" class="modal-button" id="btn-close" title="Fechar o menu de gerenciamento">Fechar</button>
+        <button type="button" class="modal-button" id="submit-generate-label" title="Gerar etiquetas">Gerar
+            etiquetas</button>
+    </x-slot name="footer">
+</x-modals.modal>
