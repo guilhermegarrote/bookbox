eaff96779eb00d9b1a7974590e9e8739c1e978bd
Guilherme Garrote <guilhermegg2008@gmail.com>
2025-10-31 21:28:15 -0300

    feat(models): adiciona funções get no CopyController
    Adiciona as funções get para o id de livro e gênero serem convertidos de binário para hexadecimal no formato UUID e também adiciona o get para retornar o ISBN formatado

--- app/Models/View/Copy.php
+++ app/Models/View/Copy.php
@@ -4,6 +4,7 @@ namespace App\Models\View;
 
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Ramsey\Uuid\Uuid;
 
 class Copy extends BaseModel
 {
@@ -11,6 +12,38 @@ class Copy extends BaseModel
     public $timestamps = false;
 
     protected $guarded = [];
+
+    public function getBookIdAttribute($value)
+    {
+        return $value ? Uuid::fromBytes($value)->toString() : null;
+    }
+
+        public function getGenreIdAttribute($value)
+    {
+        return $value ? Uuid::fromBytes($value)->toString() : null;
+    }
+
+    public function getIsbnAttribute($value): ?string
+    {
+        if (!$value) {
+            return null;
+        }
+
+        $numbersOnly = preg_replace('/\D/', '', $value);
+
+        if (strlen($numbersOnly) === 13) {
+            return sprintf(
+                '%s-%s-%s-%s-%s',
+                substr($numbersOnly, 0, 3),
+                substr($numbersOnly, 3, 1),
+                substr($numbersOnly, 4, 4),
+                substr($numbersOnly, 8, 4),
+                substr($numbersOnly, 12, 1)
+            );
+        }
+
+        return $numbersOnly;
+    }
 
     public function book(): BelongsTo
     {
