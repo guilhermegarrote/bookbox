0c65aaacc51f05549f654844f9d93d61686beeb1
Guilherme Garrote <guilhermegg2008@gmail.com>
2025-11-08 18:02:09 -0300

    refactor(config): melhora formatação dos arquivos da pasta config

--- config/app.php
+++ config/app.php
@@ -94,7 +94,7 @@ return [
     */
     'previous_keys' => [
         ...array_filter(
-            explode(',', env('APP_PREVIOUS_KEYS', ''))
+            explode(',', env('APP_PREVIOUS_KEYS', '')),
         ),
     ],
 
