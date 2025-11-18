1074365ae76c789ec09c30b23099352701221075
Guilherme Garrote <guilhermegg2008@gmail.com>
2025-11-18 14:47:18 -0300

    refactor(partials): redefine php da tabela para ser usada com scroll infinito

--- resources/views/pages/books/partials/table.blade.php
+++ resources/views/pages/books/partials/table.blade.php
@@ -1,77 +1,19 @@
-@if ($books->isEmpty())
-    <div class="data-empty">
-        <h1 title="Nenhum livro encontrado">Nenhum livro encontrado.</h1>
-    </div>
-@else
-    @php
-        function sortLink($column, $label)
-        {
-            $isCurrent = request('sort') === $column;
-            $currentDirection = request('direction', 'asc');
-            $newDirection = $isCurrent && $currentDirection === 'asc' ? 'desc' : 'asc';
-            $icon = $isCurrent ? ($currentDirection === 'asc' ? '↑' : '↓') : '';
-            $query = array_merge(request()->all(), ['sort' => $column, 'direction' => $newDirection]);
-            $url = request()->url() . '?' . http_build_query($query);
-
-            return '<a href="' .
-                e($url) .
-                '" class="sort-link" data-column="' .
-                e($column) .
-                '" data-direction="' .
-                e($newDirection) .
-                '" title="Ordenar por ' .
-                e($label) .
-                ' (' .
-                e($newDirection) .
-                ')">' .
-                '<span title="Ordenar por ' .
-                e($label) .
-                '">' .
-                e($label) .
-                ' ' .
-                $icon .
-                '</span></a>';
-        }
-    @endphp
-
+<div class="table-wrapper" id="data-table-container">
     <table class="data-table" role="table" aria-label="Tabela de livros">
-        <thead>
-            <tr>
-                <th scope="col" class="copy-col" title="Quantidade de livros disponíveis">Disponíveis</th>
-                <th scope="col" title="Número ISBN do livro">ISBN</th>
-                <th scope="col">{!! sortLink('title', 'Título') !!}</th>
-                <th scope="col">{!! sortLink('author', 'Autor') !!}</th>
-                <th scope="col" title="Gênero do livro">Gênero</th>
-                <th scope="col">{!! sortLink('publisher', 'Editora') !!}</th>
-                <th scope="col" class="button-col" style="width: 55px; padding-right: 10px">
-                    <button class="btn-dark btn-label" title="Gerar etiquetas">
-                        <x-icons.icon name="label" class="" />
-                    </button>
-                </th>
-                <th scope="col" class="button-col">
-                    <button class="btn-dark btn-add" title="Adicionar novo livro">
-                        <x-icons.icon name="plus" class="" />
-                    </button>
-                </th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($books as $book)
-                <tr data-book-id="{{ $book->id }}">
-                    <td class="copy-col" title="Disponíveis: {{ $book->available_copies }}/{{ $book->total_copies }}">
-                        {{ $book->available_copies }}
-                    </td>
-                    <td title="ISBN: {{ $book->isbn }}">{{ $book->isbn }}</td>
-                    <td title="Título: {{ $book->title }}">{{ $book->title }}</td>
-                    <td title="Autor: {{ $book->author }}">{{ $book->author }}</td>
-                    <td title="Gênero: {{ $book->genre_name }}">
-                        {{ $book->genre_name }}
-                    </td>
-                    <td title="Editora: {{ $book->publisher }}">{{ $book->publisher }}</td>
-                    <td class="button-col"></td>
-                    <td class="button-col"></td>
-                </tr>
-            @endforeach
-        </tbody>
+        <thead id="data-table-head"></thead>
+        <tbody id="book-rows"></tbody>
     </table>
-@endif
+    <div id="loader" class="loader" style="text-align:center; padding:10px; display:none;">
+        <span>Carregando mais livros...</span>
+    </div>
+</div>
+
+<div id="button-prototypes" style="display:none;">
+    <button data-icon="label" class="btn-dark btn-label" title="Gerar etiquetas">
+        <x-icons.icon name="label" />
+    </button>
+
+    <button data-icon="plus" class="btn-dark btn-add" title="Adicionar novo livro">
+        <x-icons.icon name="plus" />
+    </button>
+</div>
