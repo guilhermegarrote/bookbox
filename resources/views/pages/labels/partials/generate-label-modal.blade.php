371c6f597b1a0cd501dae854a4faf160657b0a4d
Guilherme Garrote <guilhermegg2008@gmail.com>
2025-11-23 18:41:30 -0300

    refactor(page): altera id do botão de fechar modal de gerar etiquetas

--- resources/views/pages/labels/partials/generate-label-modal.blade.php
+++ resources/views/pages/labels/partials/generate-label-modal.blade.php
@@ -3,7 +3,7 @@
     </x-slot name="content">
 
     <x-slot name="footer">
-        <button type="button" class="modal-button" id="btn-close" title="Fechar o menu de gerenciamento">Fechar</button>
+        <button type="button" class="modal-button" id="btn-close" title="Fechar o menu">Fechar</button>
         <button type="button" class="modal-button" id="submit-generate-label" title="Gerar etiquetas">Gerar
             etiquetas</button>
     </x-slot name="footer">
