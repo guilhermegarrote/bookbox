9d76f7687ce32f8826f1be54b6d5f991d6e0af92
Guilherme Garrote <guilhermegg2008@gmail.com>
2025-11-24 14:56:54 -0300

    feat(vite): adiciona alias para a pasta de js e css

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,21 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import fg from 'fast-glob';
+import path from 'path';
 
 const jsApi = fg.sync('resources/js/api/**/*.js');
 const jsComponents = fg.sync('resources/js/components/**/*.js');
 const jsPages = fg.sync('resources/js/pages/**/*.js');
 
+const cssEmails = 'resources/css/emails/style.css';
+
 export default defineConfig({
+    resolve: {
+        alias: {
+            '@css': path.resolve(__dirname, 'resources/css'),
+            '@js': path.resolve(__dirname, 'resources/js'),
+        },
+    },
     plugins: [
         laravel({
             input: [
@@ -15,6 +24,7 @@ export default defineConfig({
                 ...jsApi,
                 ...jsComponents,
                 ...jsPages,
+                cssEmails,
             ],
             refresh: true,
         }),
