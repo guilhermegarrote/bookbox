ddc9bdcf2aaec79504f521888bbd4c665c11c14c
Guilherme Garrote <guilhermegg2008@gmail.com>
2025-11-28 21:20:40 -0300

    feat(config): adiciona mais configurações do sistema que o usuário pode modificar

--- config/settings.php
+++ config/settings.php
@@ -6,6 +6,19 @@ return [
     'max_book_loans' => [
         'type' => 'integer',
         'min' => 1,
-        'max' => 4,
+        'max' => 10,
+        'default' => 3,
+    ],
+    'default_due_days' => [
+        'type' => 'integer',
+        'min' => 1,
+        'max' => 180,
+        'default' => 14,
+    ],
+    'extension_days' => [
+        'type' => 'integer',
+        'min' => 1,
+        'max' => 180,
+        'default' => 7,
     ],
 ];
