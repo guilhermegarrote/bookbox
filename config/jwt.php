a082488da1bdf26d65cb81033f794bbbe0c4e8ab
Guilherme Garrote <guilhermegg2008@gmail.com>
2026-01-29 17:35:03 -0300

    refactor(config): remove nome de cookie jwt que não é mais usado

--- config/jwt.php
+++ config/jwt.php
@@ -188,7 +188,6 @@ return [
     | Name of the cookie used to store the JWT.
     |
     */
-    'cookie_key_name' => env('JWT_COOKIE_NAME', 'jwt_token'),
     'access_cookie' => env('JWT_ACCESS_COOKIE', 'access_token'),
     'refresh_cookie' => env('JWT_REFRESH_COOKIE', 'refresh_token'),
 
