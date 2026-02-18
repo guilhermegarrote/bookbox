922ac7a00407e3e62a46ed8e8b0aa8db1c608aa4
Guilherme Garrote <guilhermegg2008@gmail.com>
2026-02-18 16:03:33 -0300

    chore(settings): remove atributos inúteis do html da lista de usuários

--- resources/views/pages/settings/partials/users-list.blade.php
+++ resources/views/pages/settings/partials/users-list.blade.php
@@ -7,11 +7,11 @@
 
             @if ($user->email == auth()->user()->email)
                 <div class="settings-actions">
-                    <button data-icon="pencil" class="btn-list-config" data-action="edit" title="Editar usuário">
+                    <button class="btn-list-config" data-action="edit" title="Editar usuário">
                         <x-icons.icon name="pencil" />
                     </button>
 
-                    <button data-icon="trash" class="btn-list-config" data-action="delete" title="Excluir usuário">
+                    <button class="btn-list-config" data-action="delete" title="Excluir usuário">
                         <x-icons.icon name="trash" />
                     </button>
                 </div>
